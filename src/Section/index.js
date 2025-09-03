2c381235b27b22ef11e567eb74256957a7d2dcab
Tomasz Krzemiński <Tomasz.krzeminski4@gmail.com>
2025-09-03 16:26:46 +0200

    cleanup

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,13 +2,13 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
-            <header className="section__header section__header--with-buttons">
-                <h2 className="section__title">{title}</h2>
-                <div className="section__buttons">
-                    {extraHeaderContent}
-                </div>
-            </header>
- <div className="section__header-line" />
+        <header className="section__header section__header--with-buttons">
+            <h2 className="section__title">{title}</h2>
+            <div className="section__buttons">
+                {extraHeaderContent}
+            </div>
+        </header>
+        <div className="section__header-line" />
         <div className="section__body">
             {body}
         </div>
