5c925d2a71b3da9c4bb8e40662696b30bc73b68b
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2025-12-13 12:02:30 +0100

    blocked  empty task adding

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
+const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        addNewTask();
-    }
+        const trimmedNewTaskContent = newTaskContent.trim();
+
+        if (!trimmedNewTaskContent) {
+            return;
+        }
+
+        addNewTask(trimmedNewTaskContent);
+        setNewTaskContent("");
+    };
 
     return (
         <form className="form" onSubmit={onFormSubmit}>
