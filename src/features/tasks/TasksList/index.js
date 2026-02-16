30ec8648513e5d6b89a20ee20008ff489cd08de6
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2026-02-16 16:45:24 +0100

    Connect TasksList to Redux

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,19 +1,22 @@
 import { List, Item, Button, Content } from "./styled";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTask, toggleTaskDone } from "../../features/tasks/tasksSlice";
 
-const TasksList = ({ hideDone, removeTask, toggleTaskDone }) => {
+const TasksList = ({ hideDone }) => {
   const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
 
   return (
     <List>
       {tasks.map((task) => (
         <Item key={task.id} hidden={task.done && hideDone}>
-          <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
+          <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
             {task.done ? "✓" : "\u00A0"}
           </Button>
           <Content done={task.done}>{task.content}</Content>
 
-          <Button remove onClick={() => removeTask(task.id)}>
+          <Button remove onClick={() => dispatch(removeTask(task.id))}>
             🗑
           </Button>
         </Item>
