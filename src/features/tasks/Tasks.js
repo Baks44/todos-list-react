c619a9f8ed292346b07de8598195090251899428
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2026-02-18 18:49:15 +0100

    refactor: complete Redux migration for tasks state

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,15 +4,8 @@ import TasksList from "./TasksList";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
 import Container from "../../common/Container";
-import useLocalStorage from "../../Hooks/useLocalStorage";
 
 function Tasks() {
-  const [hideDone, setHideDone] = useLocalStorage("hideDone", false);
-
-  const toggleHideDone = () => {
-    setHideDone((hideDone) => !hideDone);
-  };
-
   return (
     <Container>
       <Header title="Lista Zadań" />
@@ -21,10 +14,8 @@ function Tasks() {
 
       <Section
         title="Lista zadań"
-        body={<TasksList hideDone={hideDone} />}
-        extraHeaderContent={
-          <Buttons hideDone={hideDone} toggleHideDone={toggleHideDone} />
-        }
+        body={<TasksList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
