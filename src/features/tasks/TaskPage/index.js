4904dc8267de6e5b943443df29042845796144a6
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2026-02-24 06:16:07 +0100

    fix: correct task id type comparison in TaskPage

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,11 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const TaskPage = () => {
   const { id } = useParams();
 
   const task = useSelector((state) =>
-    state.tasks.tasks.find((task) => task.id === id),
+    state.tasks.tasks.find((task) => task.id === Number(id)),
   );
 
   if (!task) {
@@ -14,6 +14,8 @@ const TaskPage = () => {
 
   return (
     <div>
+      <Link to="/">← Powrót do listy</Link>
+
       <h2>{task.content}</h2>
       <p>Status: {task.done ? "Ukończone" : "Nieukończone"}</p>
     </div>
